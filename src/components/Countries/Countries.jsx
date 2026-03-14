180f1fcf1ef747f9ddaa1efcebe8dd0958afed1d
MaksumulEmon <maksumulemon300@gmail.com>
2026-03-15 02:36:30 +0600

    Show Flags and Handle data error

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,12 +1,19 @@
 import React, { use } from 'react';
+import Country from '../Country/Country';
 
-const Countries = ({countriesPromies}) => {
-    const  countriesData= use(countriesPromies);
-    const countries =countriesData.countries;
-    console.log(countries);
+const Countries = ({ countriesPromies }) => {
+
+    const countriesData = use(countriesPromies);
+    const countries = countriesData.countries;
+    // console.log(countries);
     return (
         <div>
             <h1>Hello:{countries.length}</h1>
+            {
+                countries.map(country => <Country
+                    key={country.cca3.cca3}
+                    country={country}></Country>)
+            }
         </div>
     );
 };
